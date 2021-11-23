55ba294527e5c30e15fee4390332d9c6aa658959
Nicolas Fournier <n.fournier.contact@gmail.com>
2021-11-23 15:35:29 +0100

    V11-fixed-offline

--- Restaurants/src/api/zomato.js
+++ Restaurants/src/api/zomato.js
@@ -1,13 +1,43 @@
 const API_KEY = '';
 const LONDON_ID = '61';
 
-export async function getRestaurants(searchTerm = '', offset = 0) {
+/*export async function getRestaurants(searchTerm = '', offset = 0) {
   try {
     const myHeaders = new Headers({ 'user-key': API_KEY });
     const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${LONDON_ID}&entity_type=city&start=${offset}&q=${searchTerm}`;
     const response = await fetch(url, { headers: myHeaders });
     const json = await response.json();
     return json;
+  } catch (error) {
+    console.log(`Error with function getRestaurants ${error.message}`);
+    throw error;
+  }
+};*/
+
+
+
+export async function getRestaurants(searchTerm = '', offset = 0) {
+  try {
+    let response;
+    switch (offset) {
+      case 0:
+        console.log('0');
+        response = require('../helpers/Search-start-0.json');
+        break;
+      case 20:
+        console.log('20');
+        response = require('../helpers/Search-start-20.json');
+        break;
+      case 40:
+        console.log('40');
+        response = require('../helpers/Search-start-40.json');
+        break;
+      default:
+        console.log("default");
+        response = require('../helpers/Search-start-0.json');
+    }
+    //const json = await response.json();
+    return response;
   } catch (error) {
     console.log(`Error with function getRestaurants ${error.message}`);
     throw error;
